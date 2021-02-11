1a0922c2ef8765f52997c0739846fd592f6883b9
VIctory <garubav@gmail.com>
2021-02-12 00:29:52 +0100

    add header navigation button

--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -3,25 +3,37 @@ import {View, Text, StyleSheet, FlatList, Button, TouchableOpacity} from 'react-
 import {Context} from '../context/BlogContext'
 import {Feather} from '@expo/vector-icons';
 
-const indexScreen = () => {
+const indexScreen = ({navigation}) => {
 	const {state, addBlogPost, deleteBlogPost} = useContext(Context)
 	return (<>
 		<Button onPress={addBlogPost} title={'Add Blog Post'}/>
 		<FlatList data={state}
 		          keyExtractor={blogPost => blogPost.id.toString()}
 		          renderItem={({item}) => {
-			          return (<View style={styles.rowStyle}>
-				          <Text style={styles.titleStyle}>{item.title} - {item.id}</Text>
-				          <TouchableOpacity onPress={() => {
-					          deleteBlogPost(item.id)
-				          }}>
-					          <Feather name="trash" style={styles.deleteIcon}/>
+			          return (
+				          <TouchableOpacity onPress={() => navigation.navigate('Show', {id: item.id})}>
+					          <View style={styles.rowStyle}>
+						          <Text style={styles.titleStyle}>{item.title} - {item.id}</Text>
+						          <TouchableOpacity onPress={() => {
+							          deleteBlogPost(item.id)
+						          }}>
+							          <Feather name="trash" style={styles.deleteIcon}/>
+						          </TouchableOpacity>
+					          </View>
 				          </TouchableOpacity>
-			          </View>)
+			          )
 		          }}/>
 	</>)
 }
-
+indexScreen.navigationOptions = ({navigation}) => {
+	return {
+		headerRight: () => (
+			<TouchableOpacity onPress={() => navigation.navigate('Create')}>
+				<Feather name="plus" size={30}/>
+			</TouchableOpacity>
+		),
+	};
+}
 const styles = StyleSheet.create({
 	rowStyle: {
 		flexDirection: 'row',
