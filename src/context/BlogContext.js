4b3f70125c2ed2cbd874e350d4de2e4ad909495a
VIctory <garubav@gmail.com>
2021-02-14 02:55:19 +0100

    switch to json server for data persistence

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -1,13 +1,10 @@
 import createDataContext from "./createDataContext";
+import jsonServer from "../api/jsonServer";
 
 const reducer = (state, action) => {
 	switch (action.type) {
-		case 'add_blog_post':
-			return [...state, {
-				id: Math.floor(Math.random() * 99999),
-				title: action.payload.title,
-				content: action.payload.content
-			}];
+		case 'get_blog_post':
+			return [...action.payload]
 		case 'edit_blog_post':
 			return [...state.map(blogPost => {
 				return blogPost.id === action.payload.id ? action.payload : blogPost
@@ -18,29 +15,54 @@ const reducer = (state, action) => {
 			return state
 	}
 }
-const addBlogPost = dispatch => {
-	return (title, content, callback) => {
-		dispatch({type: 'add_blog_post', payload: {title, content}})
-		if (callback) callback()
+const addBlogPost = () => {
+	return async (title, content, callback) => {
+		try {
+			await jsonServer.post('/blogPosts', {title, content})
+			if (callback) callback()
+		} catch (e) {
+			console.log(e)
+		}
 	}
 }
-
+const getBlogPost = dispatch => {
+	return async () => {
+		try {
+			const response = await jsonServer.get('/blogPosts')
+			dispatch({type: 'get_blog_post', payload: response.data})
+		} catch (e) {
+			console.log(e)
+		}
+	}
+}
 const deleteBlogPost = dispatch => {
-	return (id) => {
-		dispatch({type: 'delete_blog_post', payload: id})
+	return async (id) => {
+		try {
+			await jsonServer.delete(`/blogPosts/${id}`)
+			dispatch({type: 'delete_blog_post', payload: id})
+		} catch (e) {
+			console.log(e)
+		}
 	}
 }
 
 const editBlogPost = dispatch => {
-	return (id, title, content, callback) => {
-		dispatch({type: 'edit_blog_post', payload: {id, title, content}})
-		if (callback) callback()
+	return async (id, title, content, callback) => {
+		try {
+			await jsonServer.put(`/blogPosts/${id}`, {title, content})
+			dispatch({type: 'edit_blog_post', payload: {id, title, content}})
+
+			if (callback) callback()
+		} catch (e) {
+			console.log(e)
+		}
 	}
 }
 
 
-export const {Context, Provider} = createDataContext(reducer, {addBlogPost, deleteBlogPost, editBlogPost}, [{
-	title: 'TEST_POST',
-	content: 'TEST_CONTENT',
-	id: 1
-}])
+export const {Context, Provider} = createDataContext(reducer, {
+	addBlogPost,
+	deleteBlogPost,
+	editBlogPost,
+	getBlogPost
+}, [])
--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -1,10 +1,19 @@
-import React, {useContext} from 'react'
+import React, {useContext, useEffect} from 'react'
 import {View, Text, StyleSheet, FlatList, Button, TouchableOpacity} from 'react-native'
 import {Context} from '../context/BlogContext'
 import {Feather} from '@expo/vector-icons';
 
 const indexScreen = ({navigation}) => {
-	const {state, addBlogPost, deleteBlogPost} = useContext(Context)
+	const {state, deleteBlogPost, getBlogPost} = useContext(Context)
+	useEffect(() => {
+		getBlogPost()
+		const listener = navigation.addListener('didFocus', () => {
+			getBlogPost()
+		})
+		return () => {
+			listener.remove()
+		}
+	}, [])
 	return (<>
 		<FlatList data={state}
 		          keyExtractor={blogPost => blogPost.id.toString()}
